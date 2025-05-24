faa98cb79bda1536c010a360415615f19865a44b
FernandoSousae <fer.pe.sou.86@gmail.com>
2025-05-24 20:21:45 -0300

    Projeto AluGames

---
+++ praticando-logica-projeto_inicial/alugames/js/app.js
@@ -0,0 +1,20 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        if (confirm(`Realmente deseja devolver o jogo ${gameClicado.querySelector('.dashboard__item__name').textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';    
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+}
