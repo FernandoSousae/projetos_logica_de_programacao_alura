c8baf3d521d5f3b1ffcf1b11c1a3a276c26b1f5f
FernandoSousae <fer.pe.sou.86@gmail.com>
2025-05-25 16:09:16 -0300

    Desafio Ingressos

--- praticando-logica-projeto_inicial/ingresso/js/app.js
+++ praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -1,3 +1,44 @@
 function comprar(){
-    
+    let tipo = document.getElementById("tipo-ingresso");
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd)
+    }
+}
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para o tipo Pista');
+    } else {
+        alert('Compra realizada com sucesso');
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para o tipo Cadeira Superior');
+    } else {
+        alert('Compra realizada com sucesso');
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+    }
+}
+
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para o tipo Cadeira Inferior');
+    } else {
+        alert('Compra realizada com sucesso');
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+    }
 }
